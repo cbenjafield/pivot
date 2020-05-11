f5b6b667b6c6349e35b2df72eb158012e6b7e094
Charlie Benjafield <me@cbenjafield.com>
2020-05-11 15:18:32 +0100

    Add user to UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'Charlie Benjafield',
+            'email' => 'charlie@drivejohnsons.co.uk',
+            'password' => Hash::make('Secret123!'),
+        ]);
     }
 }
