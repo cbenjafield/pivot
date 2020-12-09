5616150db8b99f5bcc46f08a2dd454c495668b57
Charlie Benjafield <me@cbenjafield.com>
2020-12-09 15:23:02 +0000

    Header adjustments

--- resources/themes/peak/partials/header.blade.php
+++ resources/themes/peak/partials/header.blade.php
@@ -1,17 +1,19 @@
 <header class="relative border-b border-gray-200 shadow-lg z-50">
     <div class="relative">
-        <div class="h-12 bg-teal-500 flex items-center md:justify-end">
-
+        <div class="h-10 bg-teal-500 flex items-center md:justify-end">
+            <div class="text-right flex item-center justify-end space-x-4">
+                <a href="tel:01908764677" class="text-white font-semibold inline-flex space-x-2">
+                    <i class="far fa-phone-alt"></i> 01908 764 677
+                </a>
+                <a href="mailto:info@peakdriving.co.uk" class="text-white font-semibold inline-flex space-x-2">
+                    <i class="far fa-at"></i> info@peakdriving.co.uk
+                </a>
+            </div>
         </div>
         <div class="container flex items-center h-24">
             <div class="flex-1 md:flex-none md:w-1/5">
                 <div class="logo py-5">
                     {!! Theme::logo() !!}
-                    <a href="tel:01908764677" class="flex items-center font-bold text-gray-900 text-lg md:text-sm mt-2">
-                        <span class="hidden lg:inline-block">CALL:</span>
-                        <span class="lg:hidden"><i class="far fa-phone-alt"></i></span>
-                        <span class="text-teal-500 ml-1 lg:ml-3">01908 764677</span>
-                    </a>
                 </div>
             </div>
             <div class="flex items-center md:hidden">
