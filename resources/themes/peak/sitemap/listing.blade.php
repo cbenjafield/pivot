e26c32abe1dfe1a9da90e86699a313a6d9b34dd6
Charlie Benjafield <me@cbenjafield.com>
2020-12-11 12:28:56 +0000

    Title

--- resources/themes/peak/sitemap/listing.blade.php
+++ resources/themes/peak/sitemap/listing.blade.php
@@ -1,7 +1,7 @@
 <ul class="list-disc space-y-3 pl-6">
     @foreach($articles as $article)
     <li>
-        <a href="{{ $article->url }}">{{ $article->title }}</a>
+        <a href="{{ $article->url }}">{{ ! $article->isHome() ? $article->title : 'Home' }}</a>
         @if($article->children->count() > 0)
         {!! Theme::makeSitemapChildrenListing($article->children) !!}
         @endif
