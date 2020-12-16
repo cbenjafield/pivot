d9acee6e184e79f2c7b81bed502f20be07311d7d
Charlie Benjafield <me@cbenjafield.com>
2020-12-16 09:17:14 +0000

    Make the text white

--- resources/themes/peak/partials/page-header.blade.php
+++ resources/themes/peak/partials/page-header.blade.php
@@ -2,7 +2,7 @@
     <div class="container relative">
         <h1 class="text-white text-5xl font-bold">{{ $article->title }}</h1>
         @if(! empty($article->getMetaField('header_html')))
-        <div class="page-header-extra mt-4 text-lg leading-relaxed">
+        <div class="page-header-extra mt-4 text-lg leading-relaxed text-white">
             {!! $article->getMetaField('header_html') !!}
         </div>
         @endif
