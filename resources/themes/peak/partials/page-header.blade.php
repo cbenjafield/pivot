fea3edf223c5c7f2f2beaba8d64213264a1bc307
Charlie Benjafield <me@cbenjafield.com>
2020-12-16 09:14:26 +0000

    Add header Html to peak theme

--- resources/themes/peak/partials/page-header.blade.php
+++ resources/themes/peak/partials/page-header.blade.php
@@ -1,5 +1,10 @@
 <div class="page-header py-32 bg-gray-900">
     <div class="container relative">
         <h1 class="text-white text-5xl font-bold">{{ $article->title }}</h1>
+        @if(! empty($article->getMetaField('header_html')))
+        <div class="page-header-extra mt-4 text-lg leading-relaxed">
+            {!! $article->getMetaField('header_html') !!}
+        </div>
+        @endif
     </div>
 </div>
